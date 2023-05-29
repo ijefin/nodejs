e3e8d5ccd82edaa5bc1ae7887a719fa942d632fa
ijefiN <jeffersonmecmiler@hotmail.com>
2023-05-29 17:05:13 -0300

    learned about file system lib

--- 1_INTRO/2_modules/index.js
+++ 1_INTRO/2_modules/index.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 
+//core modules can be used by import file system lib
 fs.readFile("nomes.txt", "utf-8", (err, data) => {
   if (err) {
     console.log(err);
+    return;
   }
 
   console.log(data);
