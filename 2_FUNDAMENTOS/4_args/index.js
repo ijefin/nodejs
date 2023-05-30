f9f9826f0ee8706406a68a5fba3d88664760dae5
ijefiN <jeffersonmecmiler@hotmail.com>
2023-05-30 15:50:55 -0300

    feature: learned about node CLI (PROCESS Object)

---
+++ 2_FUNDAMENTOS/4_args/index.js
@@ -0,0 +1,6 @@
+console.log(process.argv);
+
+const nome = process.argv.slice(2)[0].split("=")[1];
+const idade = process.argv.slice(2)[1].split("=")[1];
+
+console.log(`Olá ${nome}, você tem ${idade} anos.`);
